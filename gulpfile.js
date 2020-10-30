cdce3cb146e4ea562c44926a2eee97ac0b43b952
Juan Carlos Muñoz <jcmunoz@itdmovil.cl>
2020-10-30 10:10:24 -0300

    update definitions

--- gulpfile.js
+++ gulpfile.js
@@ -7,28 +7,40 @@ const gif = require("gulp-if");
 const gobf = require("gulp-javascript-obfuscator");
 const debug = require("gulp-debug");
 const babel = require("gulp-babel");
-const path = ["src/**/*.js"];
+const path = require("path");
+const del = require("del");
+const rename = require("gulp-rename");
+const searchPath = ["src/**/*.js"];
 function js() {
-  return src(path, {base: "src"})
+  return src(searchPath, {base: "src"})
     .pipe(debug())
     .pipe(babel())
+    .pipe(dest("dist/"))
     .pipe(terser())
     .pipe(gif(needObfuscate, gobf({compact: true})))
+    .pipe(rename({extname: ".min.js"}))
     .pipe(dest("dist/"));
 }
 function obfuscate(path) {
   return src(path, {base: "src"})
     .pipe(debug({title: "Minimizando"}))
     .pipe(babel())
+    .pipe(dest("dist/"))
     .pipe(terser())
     .pipe(gif(needObfuscate, debug({title: "Ofuscando"})))
     .pipe(gif(needObfuscate, gobf({compact: true})))
+    .pipe(rename({extname: ".min.js"}))
     .pipe(dest("dist/"));
 }
 function watch() {
-  const watcher = w(path);
+  const watcher = w(searchPath);
   watcher.on("change", obfuscate);
   watcher.on("add", obfuscate);
+  watcher.on("unlink", filePath => {
+    const filePathFromSrc = path.relative(path.resolve("src"), filePath);
+    const destFilePath = path.resolve("dist", filePathFromSrc);
+    del.sync(destFilePath);
+  });
   return watcher;
 }
 exports.watch = watch;
